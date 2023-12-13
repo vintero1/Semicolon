1c257364254c06973660dc8082e99f3e4f6b3c8e
Vasyl Vintoniak <vasyl.vintoniak.ki.2020@lpnu.ua>
2023-12-13 18:43:34 +0200

    Add Sign Up Screen UI (android)
    * Change some style of Login Screen.

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,8 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:semicolon_project/responsive/mobile_screen_layout.dart';
 import 'package:semicolon_project/responsive/responsive_layout_screen.dart';
 import 'package:semicolon_project/responsive/web_screen_layout.dart';
+import 'package:semicolon_project/screens/login_screen.dart';
+import 'package:semicolon_project/screens/signup_screen.dart';
 import 'package:semicolon_project/utils/colors.dart';
 import 'firebase_options.dart';
 
@@ -12,17 +14,16 @@ void main() async {
   if (kIsWeb) {
     await Firebase.initializeApp(
       options: const FirebaseOptions(
-        apiKey: "AIzaSyC6CcZp1OzymQs_p7CQu30-INBnQftbTCM",
-        appId: "1:817857233059:web:445fea95bb0edf590ebf16",
-        messagingSenderId: "817857233059",
-        projectId: "test-80b49",
-        storageBucket: 'test-80b49.appspot.com'
-      ),
+          apiKey: "AIzaSyC6CcZp1OzymQs_p7CQu30-INBnQftbTCM",
+          appId: "1:817857233059:web:445fea95bb0edf590ebf16",
+          messagingSenderId: "817857233059",
+          projectId: "test-80b49",
+          storageBucket: 'test-80b49.appspot.com'),
     );
   } else {
-      await Firebase.initializeApp(
+    await Firebase.initializeApp(
       options: DefaultFirebaseOptions.android,
-     );
+    );
   }
 
   runApp(MyApp());
@@ -40,8 +41,12 @@ class MyApp extends StatelessWidget {
       theme: ThemeData.dark().copyWith(
         scaffoldBackgroundColor: mobileBackgroundColor,
       ),
-      home: const ResponsiveLayout(mobileScreenLayout: MobileScreenLayout(), webScreenLayout: WebScreenLayout(),),
+      // home: const ResponsiveLayout(
+      //   mobileScreenLayout: MobileScreenLayout(),
+      //   webScreenLayout: WebScreenLayout(),
+      // ), 
+      // home: LoginScreen(),
+      home: SignupScreen(),
     );
   }
 }
-
--- lib/screens/signup_screen.dart
+++ lib/screens/signup_screen.dart
@@ -1,24 +1,28 @@
 import "package:flutter/material.dart";
-import "package:flutter_svg/flutter_svg.dart";
+import "package:flutter_svg/svg.dart";
 import "package:semicolon_project/utils/colors.dart";
 import "package:semicolon_project/widgets/text_field_input.dart";
 
-class LoginScreen extends StatefulWidget {
-  const LoginScreen({super.key});
+class SignupScreen extends StatefulWidget {
+  const SignupScreen({super.key});
 
   @override
-  State<LoginScreen> createState() => _LoginScreenState();
+  State<SignupScreen> createState() => _SignupScreenState();
 }
 
-class _LoginScreenState extends State<LoginScreen> {
+class _SignupScreenState extends State<SignupScreen> {
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passController = TextEditingController();
+  final TextEditingController _bioController = TextEditingController();
+  final TextEditingController _usernameController = TextEditingController();
 
   @override
   void dispose() {
     super.dispose();
     _emailController.dispose();
     _passController.dispose();
+    _bioController.dispose();
+    _usernameController.dispose();
   }
 
   @override
@@ -36,32 +40,69 @@ class _LoginScreenState extends State<LoginScreen> {
               SvgPicture.asset(
                 'assets/semicolon.svg',
                 color: primaryColor,
-                height: 64,
+                height: 44,
               ),
-              
+              const SizedBox(height: 2),
+
+              // Widget to accept/show our selected photo
+              Stack(
+                children: [
+                  const CircleAvatar(
+                    radius: 54,
+                    backgroundImage: NetworkImage(
+                        'https://temp-number.sfo3.digitaloceanspaces.com/tname/images/male/74/12.jpg'),
+                  ),
+                  Positioned(
+                    bottom: 70,
+                    left: 75,
+                    child: IconButton(
+                      onPressed: () {},
+                      icon: const Icon(Icons.add_to_photos_rounded),
+                      iconSize: 21.2,
+                      color: pinkColor,
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 6),
+              // Text field username input
+              TextFieldInput(
+                textEditingController: _usernameController,
+                hintText: 'Username',
+                textInputType: TextInputType.text,
+              ),
+              const SizedBox(height: 10),
+
               // Text field email input
-              const SizedBox(height: 64),
               TextFieldInput(
                 textEditingController: _emailController,
                 hintText: 'E-mail',
                 textInputType: TextInputType.emailAddress,
               ),
-              
+
               // Text field password input
-              const SizedBox(height: 24),
+              const SizedBox(height: 10),
               TextFieldInput(
                 textEditingController: _passController,
                 hintText: 'Password',
                 textInputType: TextInputType.text,
                 isPass: true,
               ),
-              
+
+              // Text field bio input
+              const SizedBox(height: 10),
+              TextFieldInput(
+                textEditingController: _bioController,
+                hintText: 'Bio',
+                textInputType: TextInputType.text,
+              ),
+
               // Sign in button
-              const SizedBox(height: 24),
+              const SizedBox(height: 10),
               InkWell(
                 onTap: () {},
                 child: Container(
-                  width: double.infinity, 
+                  width: double.infinity,
                   alignment: Alignment.center,
                   padding: const EdgeInsets.symmetric(vertical: 12),
                   decoration: const ShapeDecoration(
@@ -71,12 +112,12 @@ class _LoginScreenState extends State<LoginScreen> {
                         ),
                       ),
                       color: pinkColor),
-                  child: const Text('Sign In'),
+                  child: const Text('Sign Up'),
                 ),
               ),
               const SizedBox(height: 12),
               Flexible(flex: 2, child: Container()),
-              
+
               // Sign up transition
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
@@ -85,7 +126,7 @@ class _LoginScreenState extends State<LoginScreen> {
                     padding: const EdgeInsets.symmetric(
                       vertical: 8,
                     ),
-                    child: const Text("New to semicolon?"),
+                    child: const Text("Already a semicoloner?"),
                   ),
                   GestureDetector(
                     onTap: () {},
@@ -94,7 +135,7 @@ class _LoginScreenState extends State<LoginScreen> {
                         vertical: 8,
                       ),
                       child: const Text(
-                        "  Sign Up.",
+                        "  Sign In.",
                         style: TextStyle(fontWeight: FontWeight.bold),
                       ),
                     ),
