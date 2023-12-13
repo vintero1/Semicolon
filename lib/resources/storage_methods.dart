359e09c0a132528817ed61083a9e93ffe3d65d92
Vasyl Vintoniak <vasyl.vintoniak.ki.2020@lpnu.ua>
2023-12-13 22:13:08 +0200

    Add storage mechanism for profile photos

---
+++ lib/resources/storage_methods.dart
@@ -0,0 +1,25 @@
+import "dart:typed_data";
+
+import "package:firebase_auth/firebase_auth.dart";
+import "package:firebase_storage/firebase_storage.dart";
+import "package:flutter/material.dart";
+
+class StorageMethods {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseStorage _storage = FirebaseStorage.instance;
+
+  // Add an image to firebase storage
+  Future<String> uploadImageToStorage(
+      String childName, Uint8List file, bool isPost) async {
+    // creating location to our firebase storage
+
+    Reference ref =
+        _storage.ref().child(childName).child(_auth.currentUser!.uid);
+
+    UploadTask uploadTask = ref.putData(file);
+
+    TaskSnapshot snapshot = await uploadTask;
+    String downloadUrl = await snapshot.ref.getDownloadURL();
+    return downloadUrl;
+  }
+}
